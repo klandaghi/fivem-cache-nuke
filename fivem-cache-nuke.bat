66031d397dffc5199cc3867f5127b1d49e838c0b
klandaghi <klandaghi@oni-neko.info>
2023-04-05 13:47:27 -0600

    Update fivem-cache-nuke.bat
    Fixed Comments

--- fivem-cache-nuke.bat
+++ fivem-cache-nuke.bat
@@ -1,13 +1,31 @@
-@ECHO OFF                                                                          :: Turns off Prompts for each line as code executed in Command Line
-C:                                                                                 :: Sets the Drive we are working with to C:\
-cd C:\Users\%username%\AppData\local\FiveM\FiveM.app\data                          :: Changes the Directory to the User's FiveM App Data Folder
-ECHO =========================================================                     :: Displays Text for the user on what step is happening next
-ECHO Clearing Default Install cache files                                          :: Displays Text for the user on what step is happening next
-ECHO =========================================================                     :: Displays Text for the user on what step is happening next
-@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\cache                :: Deletes the cache folder
-@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache         :: Deletes the servver-cache folder
-@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv    :: Deletes the server-cache-priv folder
-ECHO =========================================================                     :: Displays Text that the work has been done.
-ECHO Default Install cache files cleared                                           :: Displays Text that the work has been done.
-ECHO =========================================================                     :: Displays Text that the work has been done.
-pause                                                                              :: Pauses for User Input so they can see the resutls.
+REM Turns off Prompts for each line as code executed in Command Line
+@ECHO OFF
+
+REM Sets the Drive we are working with to C:\
+C:
+
+REM Changes the Directory to the User's FiveM App Data Folder
+cd C:\Users\%username%\AppData\local\FiveM\FiveM.app\data
+
+REM Displays Text for the user on what step is happening next
+ECHO =========================================================
+ECHO Clearing Default Install cache files
+ECHO =========================================================
+
+REM Deletes the cache folder
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\cache
+
+
+REM Deletes the servver-cache folder
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache
+
+REM Deletes the server-cache-priv folder
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv
+
+REM Displays Text that the work has been done.
+ECHO =========================================================
+ECHO Default Install cache files cleared
+ECHO =========================================================
+
+REM Pauses for User Input so they can see the resutls.
+pause
