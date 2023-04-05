c7ddc6b70147bfc934055c8521dbc41c92512184
klandaghi <klandaghi@oni-neko.info>
2023-04-05 13:43:02 -0600

    Update fivem-cache-nuke.bat
    Updated with per line comments and removed the /Q so it was verbose.

--- fivem-cache-nuke.bat
+++ fivem-cache-nuke.bat
@@ -1,13 +1,13 @@
-@ECHO OFF
-C:
-cd C:\Users\%username%\AppData\local\FiveM\FiveM.app\data
-ECHO =========================================================
-ECHO Clearing Default Install cache files
-ECHO =========================================================
-@RD /S /Q C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\cache
-@RD /S /Q C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache
-@RD /S /Q C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv
-ECHO =========================================================
-ECHO Default Install cache files cleared
-ECHO =========================================================
-pause
+@ECHO OFF                                                                          :: Turns off Prompts for each line as code executed in Command Line
+C:                                                                                 :: Sets the Drive we are working with to C:\
+cd C:\Users\%username%\AppData\local\FiveM\FiveM.app\data                          :: Changes the Directory to the User's FiveM App Data Folder
+ECHO =========================================================                     :: Displays Text for the user on what step is happening next
+ECHO Clearing Default Install cache files                                          :: Displays Text for the user on what step is happening next
+ECHO =========================================================                     :: Displays Text for the user on what step is happening next
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\cache                :: Deletes the cache folder
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache         :: Deletes the servver-cache folder
+@RD /S C:\Users\%username%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv    :: Deletes the server-cache-priv folder
+ECHO =========================================================                     :: Displays Text that the work has been done.
+ECHO Default Install cache files cleared                                           :: Displays Text that the work has been done.
+ECHO =========================================================                     :: Displays Text that the work has been done.
+pause                                                                              :: Pauses for User Input so they can see the resutls.
